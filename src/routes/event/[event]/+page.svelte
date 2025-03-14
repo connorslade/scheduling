1e160b06916173fadae6f2516a2a66d5ff358ddb
Connor Slade <connor@connorcode.com>
2025-03-14 08:32:21 -0400

    Show event sessions sectioned by start time

--- src/routes/event/[event]/+page.svelte
+++ src/routes/event/[event]/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
-  import { Crown, Edit, Import, Pencil, Plus, Search } from "@lucide/svelte";
+  import { Import, Pencil, Plus, Search } from "@lucide/svelte";
+  import { format_date_range } from "$lib/util/date";
   import Subtitle from "$lib/components/Subtitle.svelte";
   import Text from "$lib/components/Text.svelte";
-  import { format_date_range } from "$lib/util/date";
 
   let { data }: PageProps = $props();
   let { user, event, sessions } = data;
@@ -13,10 +13,12 @@
 
   let search = $state("");
   let filtered_sessions = $derived(
-    sessions.filter(
-      (x) =>
-        search == "" || x.name.toLowerCase().includes(search.toLowerCase()),
-    ),
+    sessions
+      .filter(
+        (x) =>
+          search == "" || x.name.toLowerCase().includes(search.toLowerCase()),
+      )
+      .toSorted((a, b) => a.start_time.getTime() - b.start_time.getTime()),
   );
 </script>
 
@@ -87,9 +89,20 @@
 />
 
 {#each filtered_sessions as session, i}
+  {@const time = session.start_time.getTime()}
+  {#if i == 0 || time != filtered_sessions[i - 1].start_time.getTime()}
+    <Subtitle
+      title={session.start_time.toLocaleTimeString(undefined, {
+        timeStyle: "short",
+      })}
+      level={4}
+    />
+  {/if}
+
   <div
-    class="border-gray-200 pb-4 cursor-pointer"
-    class:border-b={i !== filtered_sessions.length - 1}
+    class="border-gray-200 pb-4 mt-2 cursor-pointer"
+    class:border-b={i !== filtered_sessions.length - 1 &&
+      time == filtered_sessions[i + 1].start_time.getTime()}
   >
     <a href={`/event/${event.slug}/${session.slug}`}>
       <h3 class="text-xl font-semibold">{session.name}</h3>
